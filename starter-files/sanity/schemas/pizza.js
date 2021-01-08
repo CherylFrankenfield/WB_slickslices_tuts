e164116c95fb6e8b9a3e5468d7e247b6930a8509
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2021-01-07 20:03:27 -0800

    added toppings preview schema to pizza.js and created new veggie pizza

--- starter-files/sanity/schemas/pizza.js
+++ starter-files/sanity/schemas/pizza.js
@@ -38,6 +38,36 @@ export default {
             description: 'Price of the pizza in cents',
             validation: Rule => Rule.min(1000),
             //ToDo: Add custom input component
-        },           
+        },
+        {
+            name: 'toppings',
+            title: 'Toppings',
+            type: 'array',
+            of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+        },          
     ],
+    preview: {
+        select: {
+            title: 'name',
+            media: 'image',
+            //sanity syntax
+            topping0: 'toppings.0.name',
+            topping1: 'toppings.1.name',
+            topping2: 'toppings.2.name',
+            topping3: 'toppings.3.name',
+        },
+        prepare: ({ title, media, ...toppings }) => {
+            //^^ use REST PARAM in a function to destruct the things you want. 
+            //Use … to capture the rest of the argument into an object 
+            //1.filter undefined toppings out
+            const tops = Object.values(toppings).filter(Boolean)
+            //2.return preview object for pizza
+            return {
+                title, 
+                media, 
+                subtitle: tops.join(', '),
+            };
+        }
+
+    }
 };
