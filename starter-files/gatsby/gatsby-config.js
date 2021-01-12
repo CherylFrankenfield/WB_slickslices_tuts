5b67ecc9460d5472f3c223ad0e6090a26b201f9a
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2021-01-11 19:08:53 -0800

    add siteMetadata for graphql queries

--- starter-files/gatsby/gatsby-config.js
+++ starter-files/gatsby/gatsby-config.js
@@ -1,1 +1,9 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+// ES Modules syntax
+
+export default {
+  siteMetadata: {
+    title: 'Slicks slices',
+    siteUrl: 'https://gatsby.pizza',
+    description: 'The best pizza place in Jersey',
+  },
+};
