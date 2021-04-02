88398447eb2ba5007f226181cc250840a896794b
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2021-04-02 15:58:45 -0700

    Added CORS rule and state for response and fixed some errors by logging in and out of sanity

--- starter-files/gatsby/src/pages/index.js
+++ starter-files/gatsby/src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
 
 function CurrentlySlicing() {
   return (
@@ -16,12 +17,15 @@ function HotSlices() {
 }
 
 export default function HomePage() {
+  // const result = useLatestData();
+  const {slicemasters, hotSlices} = useLatestData();
+  // console.log(result);
   return (
     <div className="center">
       <h1>The Best Pizza Downtown!</h1>
       <p>Open 11 a.m. to 11 p.m. every single day.</p>
-      <CurrentlySlicing />
-      <HotSlices />
+      <CurrentlySlicing slicemasters={slicemasters} />
+      <HotSlices hotSlices={hotSlices} />
     </div>
   );
 }
--- starter-files/gatsby/src/utils/useLatestData.js
+++ starter-files/gatsby/src/utils/useLatestData.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
+import fetch from 'node-fetch';
 
-function useLatestData() {
+export default function useLatestData() {
   // hot slices
   const [hotSlices, setHotSlices] = useState();
   // slicemasters
@@ -8,5 +9,36 @@ function useLatestData() {
   // use a side effect to fetch the data from graphql endpoint
   useEffect(function() {
     // when the component loads, fetch the data. React hook; component mounts and runs 
+    fetch(process.env.GATSBY_GRAPHQL_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+        query {
+            StoreSettings(id: "downtown") {
+              name
+              slicemaster {
+                name
+              }
+              hotSlices {
+                name
+              }
+            }
+          }
+        `,
+      }),
+    }).then(res => res.json()).then(res => {
+        // TODO: check for errors
+        // set the data to state
+        // console.log(res.data);
+        setHotSlices(res.data.StoreSettings.hotSlices);
+        setSlicemasters(res.data.StoreSettings.slicemaster);
+      });
   }, []);
+  return {
+    hotSlices,
+    slicemasters,
+  };
 }
